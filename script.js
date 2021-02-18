e99026d96bc48f5167599962cdef2dfb0978c1fb
Brian Todd <44840863+BrianTodd092@users.noreply.github.com>
2021-02-18 08:50:20 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+var date = new Date();
+var hour = date.getHours();
+var month = date.getMonth();
+var d = date.getDate();
+
+
+
+
+
+
+// Order of if & else if, True Line, False Line.
+
+function attachEvent(){
+
+if ( month === 11){
+
+document.getElementById('x').style.display = 'block';
+}
+
+else if ( month === 0 && d < 7){
+
+document.getElementById('x').style.display = 'block';
+}
+
+else if ( month !== 11  && hour > 6 && hour < 20) {
+
+//document.getElementById('r').style.display = 'block';
+document.getElementsByClassName("ddimg")[0].className = 'dimg';
+
+}
+
+else {
+
+//document.getElementById('t').style.display = 'block';
+document.getElementsByClassName("nnimg")[0].className = 'nimg';
+
+}
+}
+
